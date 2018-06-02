9c0116d07156b98d1a63fa545c2407833663066c
hunghung166 <hungnvd00436@fpt.edu.vn>
2018-06-02 10:14:15 +0700

    Add project files.

---
+++ ExamFinal/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ExamFinal.MvcApplication" Language="C#" %>
---
+++ ExamFinal/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExamFinal.Startup))]
+namespace ExamFinal
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
